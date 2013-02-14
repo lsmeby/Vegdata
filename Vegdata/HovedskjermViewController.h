5acbb7ef047f44dbd7c7dc56ac0d5cc4d90b3455
Lars Smeby <lars@smebyfx.no>
2013-02-14 15:48:50 +0100

    Renamet noen filer

---
+++ Vegdata/HovedskjermViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Vegdata
+//
+//  Created by Lars Smeby on 14.02.13.
+//  Copyright (c) 2013 gruppe8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HovedskjermViewController : UIViewController
+
+@end
---
+++ Vegdata/HovedskjermViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Vegdata
+//
+//  Created by Lars Smeby on 14.02.13.
+//  Copyright (c) 2013 gruppe8. All rights reserved.
+//
+
+#import "HovedskjermViewController.h"
+
+@interface HovedskjermViewController ()
+
+@end
+
+@implementation HovedskjermViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
