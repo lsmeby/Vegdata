400c7cec5ee78e50faab1dd33b24e0443d19f892
Lars Smeby <lars@smebyfx.no>
2013-02-14 14:46:33 +0100

    Første commit

---
+++ Vegdata/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vegdata
+//
+//  Created by Lars Smeby on 14.02.13.
+//  Copyright (c) 2013 gruppe8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vegdata/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vegdata
+//
+//  Created by Lars Smeby on 14.02.13.
+//  Copyright (c) 2013 gruppe8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
