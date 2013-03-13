f539b99d7b89e5b21c7ad1611291271c7ad0f684
Lars Smeby <lars@smebyfx.no>
2013-03-13 17:42:43 +0100

    ?

---
+++ Vegdata/HovedskjermViewControllerLandskap.h
@@ -0,0 +1,13 @@
+//
+//  HovedskjermViewControllerLandskap.h
+//  Vegdata
+//
+//  Created by Lars Smeby on 13.03.13.
+//  Copyright (c) 2013 gruppe8. All rights reserved.
+//
+
+#import "HovedskjermViewController.h"
+
+@interface HovedskjermViewControllerLandskap : HovedskjermViewController
+
+@end
